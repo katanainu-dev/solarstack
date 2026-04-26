92e2dfb1c0b7a2a95b0fa48c379fa22f6d98179f
katanainu-dev <makevision1412@gmail.com>
2026-04-27 00:48:35 +0200

    Browser-Favicon auf Solarstack-Logo gesetzt

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="de">
       <Head>
+        <link rel="icon" type="image/png" href="/images/solarstack.png" />
+        <link rel="shortcut icon" type="image/png" href="/images/solarstack.png" />
+        <link rel="apple-touch-icon" href="/images/solarstack.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
